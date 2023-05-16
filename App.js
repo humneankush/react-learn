c58e3d504e8ed59baa747adea0f8d0fb44dbc545
humneankush <humneankush@gmail.com>
2023-05-16 18:34:26 +0530

    basics of react

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * <div id='parent>
+ * <div id= 'child'>
+ * <h1>I'm h1 tag</h1>
+ * </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i m h1 tag"),
+    React.createElement("h2", {}, "i m h2 tag"),
+  ])
+);
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//     xyz: "abc",
+//   },
+//   "Hello world from React!"
+// );
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
